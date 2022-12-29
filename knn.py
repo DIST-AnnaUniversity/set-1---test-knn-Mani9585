0928bac13c49752498987b902977b088db80d9ff
Mani9585 <58204779+Mani9585@users.noreply.github.com>
2022-12-29 15:52:18 +0530

    Update knn.py

--- knn.py
+++ knn.py
@@ -9,18 +9,23 @@ from sklearn.neighbors import KNeighborsClassifier
 ##read csv file
 dataset = pd.read_csv('soc.csv')
 ##take the values of Gender and Estimated Salary - X as a numpy array (use .values)
-X = np.array(data,columns = [
+X = data[["Gender","EstimatedSalary"]].to_numpy()
 #take y as the last column - Purchased
-
+y = data[["Purachased"]].to_numpy()
 
 #use label encoder to fit X
-
+label_encoder = preprocessing.LabelEncoder()
+data['Gender'] = label_encoder.fit_transform(data["Gender"])
+data['EstimatedSalary'] = label_encoder.fit_transform(data["EstimatedSalary"])
 #split training and testing to 80 and 20% and set the random state as 0
+X = pd.DataFrame(data,columns=['Gender','EstimatedSalary'])
+X_train, X_test, y_train, y_test = train_test_split(X,y,random_state =0)
 #use standard scaler and fit the values of X
 
 ##use the k value for neighbors as 4 with euclidean distance
-
+knn = KNeighborsClassifier(n_neigbors = 4)
 ##predict the test portion of 20%
+knn.fit(X_trian,y_train)
 
 ##how many predictions match with true values
 
