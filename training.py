ab013037bff63564cdc2250434125693d85e384e
Mani9585 <58204779+Mani9585@users.noreply.github.com>
2022-12-29 14:29:24 +0530

    Update training.py

--- training.py
+++ training.py
@@ -5,9 +5,10 @@ import numpy as np
 data = pd.read_csv('soc.csv')
 #take the values of Gender and Estimated Salary as a numpy array (use .values)
 X = pd.DataFrame(data,columns=['Gender','EstimatedSalary'])
+X = np.array(X)
 
 
 #take y as the last column - Purchased
 y = pd.DataFrame(data['Purchased'])
 #print the dimension of training set
-
+print(X.shape())
