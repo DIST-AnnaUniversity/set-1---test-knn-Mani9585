6fa5aade0ed0fb6d679d530c0f94a09eea16b715
Mani9585 <58204779+Mani9585@users.noreply.github.com>
2022-12-29 15:20:41 +0530

    Update training.py

--- training.py
+++ training.py
@@ -6,6 +6,6 @@ data = pd.read_csv('soc.csv')
 #take the values of Gender and Estimated Salary as a numpy array (use .values)
 X = data[['Gender','EstimatedSalary']].to_numpy()
 #take y as the last column - Purchased
-y = data[['purchased']].to_numpy()
+y = data[['Purchased']].to_numpy()
 #print the dimension of training set
 print(X.ndim(),y.ndim())
